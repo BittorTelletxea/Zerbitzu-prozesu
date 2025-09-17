f4445f60cceb702be3fc2e698ca1757e98035d07
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-09-17 09:44:27 +0200

    Prozesu FTP

---
+++ Ariketa1/Ariketa1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ariketa1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private Process procesoFTP;
+        private void lanzar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                procesoFTP = new Process();
+                procesoFTP.StartInfo.FileName = "ftp.exe";
+                procesoFTP.Start();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Error al iniciar el proceso: " + ex.Message);
+            }
+        }
+
+        private void matar_Click(object sender, RoutedEventArgs e)
+        {
+            if(procesoFTP != null)
+            {
+                procesoFTP.Kill();
+                label.Content = "Processo FTP eliminado";
+                procesoFTP = null;
+            }
+            else
+            {
+                label.Content = "No hay procesos FTP activos";
+            }
+        }
+
+        private void mostrar_Click(object sender, RoutedEventArgs e)
+        {
+            combo.Items.Clear();
+            Process[] procesos = Process.GetProcesses();
+            foreach(Process p in procesos)
+            {
+                combo.Items.Add(p.ProcessName);
+            }
+        }
+    }
+}

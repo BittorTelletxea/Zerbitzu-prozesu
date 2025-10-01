6eff242ba7ef87a231e03ae019481d384d604974
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-10-01 14:27:21 +0200

    Hariak

---
+++ Hariak/1.Ariketa/Adibidea/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread thread1 = new Thread(Aimar);
+        Thread thread2 = new Thread(Nerea);
+        Thread thread3 = new Thread(Jurgi);
+
+        thread1.Start();
+        thread2.Start();
+        thread3.Start();
+
+        thread3.Join();
+        thread2.Join();
+        thread1.Join();
+
+
+        Console.WriteLine("Main amaitua");
+    }
+    public static void Aimar()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Aimar " + (i+1) +" aldiz");
+            Thread.Sleep(300);
+        }
+    }
+    public static void Nerea()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Nerea " + (i + 1) + " aldiz");
+            Thread.Sleep(1000);
+        }
+    }
+    public static void Jurgi()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Jurgi " + (i + 1) + " aldiz");
+            Thread.Sleep(500);
+        }
+    }
+}
---
+++ Hariak/2.Ariketa/2.Ariketa/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Thread batuketaTh = new Thread(batuketa);
+        Thread biderketaTh = new Thread(biderketa);
+        batuketaTh.Start();
+        biderketaTh.Start();
+    }
+    static void batuketa()
+    {
+        Thread.Sleep(300);
+        Console.WriteLine(2 + 2);
+    }
+    static void biderketa()
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine(2 * 2);
+    }
+}
